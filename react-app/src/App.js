a9143b76646f87ab348eb5eb873b5a03f4a476cf
Ajith Komuravelly <ajithkomuravelly111@gmail.com>
2022-11-17 17:56:35 +0000

    final commit

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,7 +1,23 @@
+import { Table } from "react-bootstrap";
+import axios from "axios";
+import { useQuery } from "react-query";
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const { isLoading, error, data } = useQuery("posts", () =>
+    axios("https://api.covalenthq.com/v1/1/xy=k/uniswap_v2/tokens/address/0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2/transactions/?quote-currency=USD&format=JSON&page-size=5&key=ckey_dd79741ebac5474fa3c08b53f0c")
+  );
+
+  if (isLoading) {
+    <main>
+      <h2>data loading</h2>
+    </main>
+  }
+
+  console.log(data)
+
   return (
     <main>
       <Table responsive bordered hover className="rounded">
@@ -16,7 +32,7 @@ function App() {
           </tr>
         </thead>
         {
-          data.map((record) => {
+          data.data.items.map((record) => {
             return <tbody>
               <tr>
                 <td>{record.act}</td>
